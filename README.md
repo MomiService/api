7330c70d6e9239c50ec5b2856d5602b4727328bb
MomiService <momiserviceclinical@gmail.com>
2024-07-25 16:58:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+![Logo](https://miro.medium.com/v2/resize:fit:900/1*rVjj_JOGVJIS8pGEJVrLww.png)
+
+# Mockup: Mockup Nodejs Typescript Express
+
+Template for Node js API with typescript, express, with eslint configured, Jest for unit tests and supertes for the API, this code is adjusted to the default configuration of all my projects using this stock.
+
+Developed by [Daniel Campaz](https://github.com/DanielCampaz), I hope it helps you
+
+# Enviroments:
+
+Change .enviroment for .env or .env.local
+
+# Tsoa:
+
+Need generate Routes in package.json "build:tsoa" generate Routes and Swagger.json
---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import express, {
+  urlencoded,
+  Response as ExResponse,
+  Request as ExRequest,
+} from "express";
+import cors from "cors";
+import { RegisterRoutes } from "../routes/routes";
+import swaggerUi from "swagger-ui-express";
+
+export function main() {
+  const app = express();
+  const port = 3000;
+
+  app.use(
+    urlencoded({
+      extended: true,
+    })
+  );
+  app.use(cors());
+  app.use(express.json());
+
+  app.use(
+    "/docs",
+    swaggerUi.serve,
+    async (_req: ExRequest, res: ExResponse) => {
+      return res.send(
+        swaggerUi.generateHTML(await import("../annotation/swagger.json"))
+      );
+    }
+  );
+
+  RegisterRoutes(app);
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
